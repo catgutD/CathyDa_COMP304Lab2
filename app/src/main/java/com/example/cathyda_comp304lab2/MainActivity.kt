24088d20d5e555193a8f68f33af16b3bcc213820
CDA <cathyda97@gmail.com>
2024-06-10 21:42:43 -0400

    basic hookup of main screen to home types screen

--- app/src/main/java/com/example/cathyda_comp304lab2/MainActivity.kt
+++ app/src/main/java/com/example/cathyda_comp304lab2/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.cathyda_comp304lab2
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnEnter: Button = findViewById<View>(R.id.btnEnter) as Button
+        btnEnter.setOnClickListener{
+                val intent = Intent(this, HomeTypesActivity::class.java)
+                startActivity(intent)
         }
+
     }
+
 }
