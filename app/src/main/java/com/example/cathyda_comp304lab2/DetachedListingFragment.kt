b80d505638707a5ea5ec8aeeea287e46fd7ec2b2
CDA <cathyda97@gmail.com>
2024-06-17 01:17:13 -0400

    updated logo to higher quality, added in images for apartment and detached homes, set fixed dimens for fragment listings, and added button to availability screen

--- app/src/main/java/com/example/cathyda_comp304lab2/DetachedListingFragment.kt
+++ app/src/main/java/com/example/cathyda_comp304lab2/DetachedListingFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class DetachedListingFragment : Fragment() {
@@ -15,5 +16,48 @@ class DetachedListingFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_detached_home_listing, container, false)
         return view
     }
+    override fun onStart() {
+        super.onStart()
 
+        setApartmentInfo(
+            R.array.detached_home_1,
+            R.id.txtPrice1,
+            R.id.txtAddress1,
+            R.id.txtBedrooms1,
+            R.id.txtBathrooms1
+        )
+        setApartmentInfo(
+            R.array.detached_home_2,
+            R.id.txtPrice2,
+            R.id.txtAddress2,
+            R.id.txtBedrooms2,
+            R.id.txtBathrooms2
+        )
+        setApartmentInfo(
+            R.array.detached_home_3,
+            R.id.txtPrice3,
+            R.id.txtAddress3,
+            R.id.txtBedrooms3,
+            R.id.txtBathrooms3
+        )
+    }
+    fun setApartmentInfo(detachedId: Int, priceId: Int, addressId: Int, bedroomId: Int, bathroomId: Int){
+        val detachedInfo: Array<String> = resources.getStringArray(detachedId)
+
+        val price: String = detachedInfo[0]
+        val priceView: TextView? = view?.findViewById(priceId)
+        priceView?.text = price
+
+        val address: String = detachedInfo[1]
+        val addressView: TextView? = view?.findViewById(addressId)
+        addressView?.text = address
+
+        val bedrooms: String = detachedInfo[2]
+        val bedroomsView: TextView? = view?.findViewById(bedroomId)
+        bedroomsView?.text = bedrooms
+
+        val bathrooms: String = detachedInfo[3]
+        val bathroomsView: TextView? = view?.findViewById(bathroomId)
+        bathroomsView?.text = bathrooms
+    }
 }
