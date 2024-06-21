a7dd5d3081110b145e63dc06cb9b0e4ab09752d8
CDA <cathyda97@gmail.com>
2024-06-21 18:16:39 -0400

    updated text input to the correct type and start of validation

--- app/src/main/java/com/example/cathyda_comp304lab2/CardInfoActivity.kt
+++ app/src/main/java/com/example/cathyda_comp304lab2/CardInfoActivity.kt
@@ -2,11 +2,34 @@ package com.example.cathyda_comp304lab2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.Toast
 
 class CardInfoActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_info)
+
     }
 
+    fun validateInput(){
+        val fullName: EditText = findViewById(R.id.txtInputFullName)
+        val cardNumber: EditText = findViewById(R.id.txtInputNumber)
+        val answer1: EditText = findViewById(R.id.txtInputAnswer1)
+        val answer2: EditText = findViewById(R.id.txtInputAnswer2)
+        val answer3: EditText = findViewById(R.id.txtInputAnswer3)
+
+        if(fullName.text.isEmpty()){
+            Toast.makeText(this, R.string.empty_fullname_input_alert, Toast.LENGTH_LONG)
+                 .show()
+        }
+        if(cardNumber.text.isEmpty()){
+            Toast.makeText(this, R.string.empty_cardnumber_input_alert, Toast.LENGTH_LONG)
+                 .show()
+        }
+        if(answer1.text.isEmpty() or answer2.text.isEmpty() or answer3.text.isEmpty()){
+            Toast.makeText(this, R.string.empty_answer_alert, Toast.LENGTH_LONG)
+                 .show()
+        }
+    }
 }
